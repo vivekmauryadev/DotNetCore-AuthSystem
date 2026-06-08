f6a58e1a14e04ed68434984645264e900da21ee5
VIVEK-FSPL <fspl.vivekmaurya99@gmail.com>
2026-06-09 01:33:01 +0530

    Added domain entities for authentication system

---
+++ AuthSystem.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Domain.Entities
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Token { get; set; } = string.Empty;
+
+        public DateTime ExpiryDate { get; set; }
+
+        public bool IsRevoked { get; set; }
+    }
+}
