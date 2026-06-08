28f5f4482bcc81a0cd5c4f1c768415151d236b6d
VIVEK-FSPL <fspl.vivekmaurya99@gmail.com>
2026-06-09 01:43:33 +0530

    Configured database context and initial migration

--- AuthSystem.API/Program.cs
+++ AuthSystem.API/Program.cs
@@ -1,3 +1,6 @@
+using AuthSystem.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
---
+++ AuthSystem.Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using AuthSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Persistence.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+
+        public DbSet<Role> Roles => Set<Role>();
+
+        public DbSet<UserRole> UserRoles => Set<UserRole>();
+
+        public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId);
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(x => x.Role)
+                .WithMany()
+                .HasForeignKey(x => x.RoleId);
+        }
+    }
+}
